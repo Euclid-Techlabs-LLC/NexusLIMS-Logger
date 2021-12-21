91613e7043e5661e8eedb3de6a426d37c84dd304
Weinan Si <siweinan@gmail.com>
2021-12-21 17:17:34 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -187,6 +187,8 @@ else:
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+def setup(app):
+    app.add_stylesheet("css/theme.css")
 
 html_context = {
     'css_files': [
